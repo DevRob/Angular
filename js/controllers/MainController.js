7aef9d363a453d1b9bf65c1e5040b12e0849f5e2
Robert Molnar <mail.robertmolnar@gmail.com>
2016-11-24 19:19:20 +0000

    daily upd

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -5,14 +5,6 @@ app.controller('MainController', ['$scope', function($scope) {
 
   $scope.showPage = function(index) {
     $scope.actualPage = $scope.pages[index];
-
-    // var theta = 0,
-    // carousel = document.getElementById('carousel'),
-    // panelCount = carousel.children.length,
-    // transformProp = Modernizr.prefixed('transform');
-    //
-    // theta += ( 360 / panelCount ) * index * -1;
-    // carousel.style[ transformProp ] = 'translateZ( -288px ) rotateY(' + theta + 'deg)';
   };
 
 }])
